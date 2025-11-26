3b4ee6fbf3b4157a94aa3662e99739a11e5215db
user <user@unknown0ad546ce03b8.home>
2025-11-26 13:40:02 +0100

    Fix: Remove static export for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
   },
@@ -8,4 +7,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
